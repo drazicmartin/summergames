21f3ab5c7ff6cf0539ec3d65ce490856ff12247c
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-04-20 23:52:38 +0200

    feat: add tailwind conf

---
+++ front/tailwind.config.ts
@@ -0,0 +1,75 @@
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+
+  theme: {
+		extend: {
+			animation: {
+				// ...other animations
+				first: 'moveVertical 30s ease infinite',
+				second: 'moveInCircle 20s reverse infinite',
+				third: 'moveInCircle 40s linear infinite',
+				fourth: 'moveHorizontal 40s ease infinite',
+				fifth: 'moveInCircle 20s ease infinite'
+			},
+			keyframes: {
+				// ... other keyframes
+				moveHorizontal: {
+					'0%': {
+						transform: 'translateX(-50%) translateY(-10%)'
+					},
+					'50%': {
+						transform: 'translateX(50%) translateY(10%)'
+					},
+					'100%': {
+						transform: 'translateX(-50%) translateY(-10%)'
+					}
+				},
+				moveInCircle: {
+					'0%': {
+						transform: 'rotate(0deg)'
+					},
+					'50%': {
+						transform: 'rotate(180deg)'
+					},
+					'100%': {
+						transform: 'rotate(360deg)'
+					}
+				},
+				moveVertical: {
+					'0%': {
+						transform: 'translateY(-50%)'
+					},
+					'50%': {
+						transform: 'translateY(50%)'
+					},
+					'100%': {
+						transform: 'translateY(-50%)'
+					}
+				}
+			}
+		}
+	},
+
+	plugins: [
+		forms,
+		skeleton({
+			themes: { preset: [ "skeleton" ] }
+		})
+	]
+};
