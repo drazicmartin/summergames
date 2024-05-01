8f960c8fbfeebfd3a2f68557990879976c0502df
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-05-01 20:04:28 +0200

    feat: docker now working

--- front/src/hooks.server.ts
+++ front/src/hooks.server.ts
@@ -3,9 +3,16 @@ import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/publi
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 import { redirect, type Handle } from '@sveltejs/kit'
 
+import fs from "fs";
+
+// detect if we're running inside docker and set the backend accordingly
+const supabase_url = fs.existsSync("/.dockerenv")
+  ? "http://supabase-kong:8000" // docker-to-docker
+  : PUBLIC_SUPABASE_URL; // localhost-to-localhost
+
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
-    supabaseUrl: PUBLIC_SUPABASE_URL,
+    supabaseUrl: supabase_url,
     supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
     event,
   })
