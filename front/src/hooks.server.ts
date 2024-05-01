b1f4bcd59419ead7d302c31d8044c789d3b3111f
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-05-02 01:23:16 +0200

    fix: test

--- front/src/hooks.server.ts
+++ front/src/hooks.server.ts
@@ -7,6 +7,8 @@ import fs from "fs";
 
 const SUPABASE_ANON_KEY = process.env['PUBLIC_SUPABASE_ANON_KEY'] ?? PUBLIC_SUPABASE_ANON_KEY
 
+console.log(SUPABASE_ANON_KEY);
+
 // detect if we're running inside docker and set the backend accordingly
 const supabase_url = fs.existsSync("/.dockerenv")
   ? "http://supabase-kong:8000" // docker-to-docker
