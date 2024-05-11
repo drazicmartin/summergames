d78f17e24fdee1d6e60760a8d24d2272cfb25d83
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2024-05-11 17:43:20 +0200

    feat: add main info on home page + reformat display

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -10,19 +10,24 @@
 	<div
 		class="pointer-events-none absolute inset-0 z-50 flex items-center justify-center px-4 text-center text-3xl font-bold text-white md:text-4xl lg:text-7xl"
 	>
-		<div class="flex flex-col w-full">
-			<div class="grid h-20">
+		<div class="flex flex-col h-full w-full justify-around">
+			<div class="grid mt-32">
 				<h1
 					class="text-7xl text-white drop-shadow-[2px_2.2px_5.2px_rgba(0,0,0,0.8)]"
 				>
 					Summer Games
 				</h1>
 			</div> 
-			<br>
-			<br>
-			<br>
-			<div class="grid h-20 justify-center">
+			<div class="grid justify-center">
 				<Countdown/>
+			</div>
+			<div class="grid">
+				<div role="alert" class="alert text-3xl">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+					<span>Vendredi 30 Août</span>
+					<span>61 impasse des croix Bigeolles</span>
+					<span></span>
+				</div>
 			</div>
 	  	</div>
 	</div>
