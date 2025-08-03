ff6356f3eff1c65d6b9a085c738e7e35aa95f2b6
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2025-08-03 13:09:30 +0200

    fix: typo

--- front/src/routes/info/+page.svelte
+++ front/src/routes/info/+page.svelte
@@ -8,7 +8,7 @@
     import GiLightBulb from 'svelte-icons/gi/GiLightBulb.svelte'
 </script>
 
-<div class="mt-32 flex justify-center i tems-center">
+<div class="mt-32 flex justify-center items-center">
     <div class="w-4/5">
         <Accordion>
             <AccordionItem open>
@@ -28,10 +28,10 @@
                 {#snippet content()}
               
                       <p class="text-justify">
-                          C'est un événement amusant et compétitif entre amis sur un weekend !<br>
-                          L'idée général c'est que <b>deux</b> équipes s'affrontent en jouant à des Jeux/Activités que l'on a organisés.
-                          À la fin du weekend, l'équipe avec le plus de points remporte le titre de <b>Vainqueur des Summer Games !</b> <br><br>
-                          En plus de participer à la compétition, vous aurez également la possibilité de jouer à d'autres jeux en libre services à tout moment le temps que l'on organise votre prochain défi !.
+                          C'est un événement divertissant et compétitif entre amis pendant un week-end!<br>
+                          L'idée générale c'est que <b>deux</b> équipes s'affrontent en jouant à des Jeux/Activités que l'on a organisés.
+                          À la fin du week-end, l'équipe avec le plus de points remporte le titre de <b>Vainqueur des Summer Games !</b> <br><br>
+                          En plus de participer à la compétition, vous aurez également la possibilité de jouer à d'autres jeux en libre-service à tout moment le temps que l'on organise votre prochain défi !.
                       </p>
                   
               {/snippet}
@@ -51,7 +51,7 @@
               
                       <p class="text-justify">
                           La compétition sera composée de jeux stratégiques, à mémoire, physiques et créatifs. <br><br>
-                          Certains jeux seront issus de l'édition précédente mais d'autre seront inédits ! Pour les nouveaux participants, nous préférons ne pas donner de détails sur les jeux pour que vous ayez la surprise.
+                          Certains jeux seront issus de l'édition précédente mais d'autres seront inédits ! Pour les nouveaux participants, nous préférons ne pas donner de détails sur les jeux pour que vous ayez la surprise.
                       </p>
                   
               {/snippet}
@@ -71,10 +71,10 @@
                 {#snippet content()}
               
                       <p class="text-justify">
-                          On s'occupe (presque) de tout, achats de l'équipement pour les activités, les boissons, les repas. Vous n'avez rien a apporter à ce niveau là. En échange on vous demande une contribution de 30€ afin d'acheter tout ce qu'il vous faut. Le but est simplement d'organiser un weekend sympa entre amis ! <br><br>
+                          On s'occupe (presque) de tout, achats de l'équipement pour les activités, les boissons, les repas. Vous n'avez rien a apporter à ce niveau là. En échange on vous demande une contribution de 30€ afin d'acheter tout ce qu'il vous faut. Le but est simplement d'organiser un week-end sympa entre amis ! <br><br>
                           Vous devrez cependant venir avec votre matos de camping, tentes, matelas, sacs de couchage, (lampe frontale ?), et tout autre équipement nécessaire pour assurer votre confort pendant votre séjour en plein air.
                           <br>
-                          Rendez-vous le vendredi <b>28 août</b> à partir de <b>16h</b> jusqu'au dimanche <b>30 août</b> après-midi à Lieurey, 61 impasse des Croix bigeolles 27560. (En normandie) 
+                          Rendez-vous le vendredi <b>28 août</b> à partir de <b>16h</b> jusqu'au dimanche <b>30 août</b> après-midi à Lieurey, 61 impasse des Croix bigeolles 27560. (En Normandie)
                       </p>
                   
               {/snippet}
@@ -93,10 +93,10 @@
                 {#snippet content()}
               
                       <p class="text-justify">
-                          Pour rendre les Summer Games possible nous vous demandons un participation financière de <b>30€</b>.<br><br>
+                          Pour rendre les Summer Games possible nous vous demandons une participation financière de <b>30€</b>.<br><br>
                           Cette participation nous permet de couvrir les frais d'achats de nourriture et de boissons, ainsi qu'une partie de l'achat de matériel pour les jeux. <br><br>
-                          Même si on aura prévu la plus par des choses à l'avance, certaine resterons à faire le jour J. Tel que l'installation des jeux, la préparation de la nourriture, un peu de rangement, de la logistique, etc... <br><br>
-                          Votre aide est grandemet la bienvenue pendant le week-end !
+                          Même si on aura prévu la plupart des choses à l'avance, certaines resteront à faire le jour J. Tel que l'installation des jeux, la préparation de la nourriture, un peu de rangement, de la logistique, etc... <br><br>
+                          Votre aide est grandement la bienvenue pendant le week-end !
                       </p>
                   
               {/snippet}
--- front/src/routes/photo/+page.svelte
+++ front/src/routes/photo/+page.svelte
@@ -13,10 +13,10 @@
 <div class="mt-32 mx-5 mb-5">
     <div class="mb-5 text-center">
         <div class="text-3xl font-bold text-center">
-            Quelque photos des éditions précédantes
+            Quelques photos des éditions précédentes
         </div>
         <div class="text-lg">
-            Toutes les photos sont disponibles au télécahrgement en cliquant sur le bouton en dessous, n'hésitez pas à téléverser vos photos pour les ajouter à la galerie.
+            Toutes les photos sont disponibles au téléchargement en cliquant sur le bouton en dessous, n'hésitez pas à téléverser vos photos pour les ajouter à la galerie.
         </div>
         <button type="button" class="btn mt-3">
             <a class="link" href="https://immich.summer-games.fr/share/Hjt6xEY5CjfQFY1sYZ7iXAM2mSknwvMoiMPuuLPxUpEhyUItjiop2-0NOrW1q-SrSR8">toute les Photos & Vidéo</a>
